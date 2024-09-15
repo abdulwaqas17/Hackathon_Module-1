e583f7a0788ac3b075b330e319b5d1f726808ea4
Abdul Waqas <www.waqas7172@gmail.com>
2024-09-15 13:08:35 +0500

    add some sections

---
+++ app.js
@@ -0,0 +1,103 @@
+// ------------ showOne ---------
+function showOne(){
+
+    let para = document.getElementById('FAQS-paraOne');
+    para.innerHTML = `
+    
+    
+    <p>
+
+    During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements.
+
+    </p>
+
+    `;
+
+}
+
+
+
+
+// ------------ showTwo ---------
+function showTwo(){
+
+    let para =  `
+    
+    <p>
+
+    During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements.
+
+    </p>
+
+    `;
+
+    document.getElementById('FAQS-paraTwo').innerHTML = para;
+
+}
+
+
+// ------------ showThree ---------
+function showThree(){
+
+    document.getElementById('FAQS-paraThree').innerHTML = `
+    
+    <p>
+
+    During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements.
+
+    </p>
+
+    `;
+
+
+}
+
+
+
+// ------------ showFour ---------
+function showFour(){
+
+    var para = document.getElementById('FAQS-paraFour');
+    para.innerHTML = `
+    
+    
+    <p>
+
+    During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements.
+
+    </p>
+
+    `;
+
+}
+
+
+
+// ------------ showFive ---------
+function showFive(){
+
+    var para = document.getElementById('FAQS-paraFive');
+    para.innerHTML = `
+    
+    
+    <p>
+
+    During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements.
+
+    </p>
+
+    `;
+
+    
+
+}
+
+function removal(){
+
+    document.getElementById('FAQS-paraFive').innerHTML= `a`;
+
+}
+
+// const para = document.getElementById("five");
+// const five = document.getElementById("FAQS-paraFive");
+// five.addEventListener("click" , () => {FAQS-paraFive.classList.toggle("active")})
